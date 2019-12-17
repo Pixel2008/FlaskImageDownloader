61c0d915f947d1f3ac09652260160d75d4de6fae
Tomasz Zakręcki <tzakrecki@gmail.com>
2019-12-17 22:11:28 +0100

    New project

---
+++ main.py
@@ -0,0 +1,11 @@
+#! python3
+'''
+Sample script for downloading all images from website
+Pixel2008 All Rights Reserved ®
+'''
+if __name__ == "__main__":
+    try:
+        print("Start")
+
+    except Exception as e2:
+        print("Error occured e2 = " + format(e2))
